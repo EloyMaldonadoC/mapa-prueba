78e93d98545e767ba695f7ff8fe0758ea5e14ef2
Eloy <eloy.maldonado@uaz.edu.mx>
2023-05-27 07:21:17 -0600

    version 0.0.2

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
+
+const { width, height } = Dimensions.get('screen');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView style={{width: width, height: height}}/>
       <StatusBar style="auto" />
     </View>
   );
